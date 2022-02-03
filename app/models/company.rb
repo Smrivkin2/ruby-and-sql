28b68997cfc2c166fbd5eeb01d6b78312da31f5b
Smrivkin2 <stephanie.rivkin@kellogg.northwestern.edu>
2022-02-03 03:13:56 +0000

    Associations Lab

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ApplicationRecord #pulling ORM into our class so it can understand to do more
+    has_many :contacts
+
+    # def contacts
+    #         #puts "hello!"
+    #         Contact.where({ company_id: id})
 end
